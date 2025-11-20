11c4e2993ac3d92b0e9637d0d47efb5f79c56ada
Michael Khuri <michaelkhuri@gmail.com>
2025-11-20 12:14:03 -0800

    update pip install psycopg2-binary

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 with app.app_context():
     db.create_all()
+    print("Tables created")
 
 class Dealership(db.Model):
     __tablename__ = "dealerships"
